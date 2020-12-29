c8c3418c3af901500c736a9f3608af0f0cf4bd5f
danielbr08@gmail.com <Dan136901>
2020-12-29 21:27:58 +0200

    wharehouse-unit table add inputs

--- src/app/emergency-warehouse-unit-inventory/emergency-warehouse-unit-inventory.component.ts
+++ src/app/emergency-warehouse-unit-inventory/emergency-warehouse-unit-inventory.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 
 export interface PeriodicElement {
   pakal: string;
@@ -7,16 +7,16 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {id: 1, pakal: 'Hydrogen', quantity: 1.0079},
-  {id: 2, pakal: 'Helium', quantity: 4.0026},
-  {id: 3, pakal: 'Lithium', quantity: 6.941},
-  {id: 4, pakal: 'Beryllium', quantity: 9.0122},
-  {id: 5, pakal: 'Boron', quantity: 10.811},
-  {id: 6, pakal: 'Carbon', quantity: 12.0107},
-  {id: 7, pakal: 'Nitrogen', quantity: 14.0067},
-  {id: 8, pakal: 'Oxygen', quantity: 15.9994},
-  {id: 9, pakal: 'Fluorine', quantity: 18.9984},
-  {id: 10, pakal: 'Neon', quantity: 20.1797},
+  {id: 1, pakal: 'Hydrogen', quantity: 1},
+  {id: 2, pakal: 'Helium', quantity: 2},
+  {id: 3, pakal: 'Lithium', quantity: 3},
+  {id: 4, pakal: 'Beryllium', quantity: 4},
+  {id: 5, pakal: 'Boron', quantity: 5},
+  {id: 6, pakal: 'Carbon', quantity: 6},
+  {id: 7, pakal: 'Nitrogen', quantity: 7},
+  {id: 8, pakal: 'Oxygen', quantity: 8},
+  {id: 9, pakal: 'Fluorine', quantity: 9},
+  {id: 10, pakal: 'Neon', quantity: 10},
 ];
 
 @Component({
