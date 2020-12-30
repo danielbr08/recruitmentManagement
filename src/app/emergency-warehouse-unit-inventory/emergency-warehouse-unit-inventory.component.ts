8b93c1cc3e2c80bc3f4aed3f6b3354d08c183c66
Daniel <daniel@myfxbook.com>
2020-12-30 12:47:38 +0200

    save warehouse table data

--- src/app/emergency-warehouse-unit-inventory/emergency-warehouse-unit-inventory.component.ts
+++ src/app/emergency-warehouse-unit-inventory/emergency-warehouse-unit-inventory.component.ts
@@ -7,16 +7,16 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {id: 1, pakal: 'Hydrogen', quantity: 1},
-  {id: 2, pakal: 'Helium', quantity: 2},
-  {id: 3, pakal: 'Lithium', quantity: 3},
-  {id: 4, pakal: 'Beryllium', quantity: 4},
-  {id: 5, pakal: 'Boron', quantity: 5},
-  {id: 6, pakal: 'Carbon', quantity: 6},
-  {id: 7, pakal: 'Nitrogen', quantity: 7},
-  {id: 8, pakal: 'Oxygen', quantity: 8},
-  {id: 9, pakal: 'Fluorine', quantity: 9},
-  {id: 10, pakal: 'Neon', quantity: 10},
+  { id: 1, pakal: 'Hydrogen', quantity: 1 },
+  { id: 2, pakal: 'Helium', quantity: 2 },
+  { id: 3, pakal: 'Lithium', quantity: 3 },
+  { id: 4, pakal: 'Beryllium', quantity: 4 },
+  { id: 5, pakal: 'Boron', quantity: 5 },
+  { id: 6, pakal: 'Carbon', quantity: 6 },
+  { id: 7, pakal: 'Nitrogen', quantity: 7 },
+  { id: 8, pakal: 'Oxygen', quantity: 8 },
+  { id: 9, pakal: 'Fluorine', quantity: 9 },
+  { id: 10, pakal: 'Neon', quantity: 10 },
 ];
 
 @Component({
@@ -32,6 +32,13 @@ export class EmergencyWarehouseUnitInventoryComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  saveData(warehouseUnitTable: any) {
+    for (let i = 0; i < warehouseUnitTable._data.length; i++) {
+      let row = warehouseUnitTable._data[i];
+      console.log("row", row);
+    }
   }
 
 }
