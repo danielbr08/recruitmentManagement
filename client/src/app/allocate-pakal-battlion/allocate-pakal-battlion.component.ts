d5c3bad8eb514ab1ab2b61b6c24dabea20abdafa
Daniel <daniel@myfxbook.com>
2021-04-07 12:46:54 +0300

    .

--- client/src/app/allocate-pakal-battlion/allocate-pakal-battlion.component.ts
+++ client/src/app/allocate-pakal-battlion/allocate-pakal-battlion.component.ts
@@ -10,25 +10,25 @@ import { RequestsService } from '../services/requests.service';
 export class AllocatePakalBattlionComponent implements OnInit {
   displayedColumns: string[] = ['pakal', 'warehousePakal', 'squad1', 'squad2', 'squad3', 'support', 'headquarters', 'total'];
   pakalAllocated: PakalAllocated[]=  [
-// { headquartersAllocated: 0,
-//   headquartersTotal: 0,
-//   pakalId: 3,
-//   pakalName: "פקל 2",
-//   squad1Allocated: 0,
-//   squad1Total: 1,
-//   squad2Allocated: 0,
-//   squad2Total: 2,
-//   squad3Allocated: 0,
-//   squad3Total: 3,
-//   supportAllocated: 0,
-//   supportTotal: 0,
-//   taskId: 3,
-//   warehouseTotal: 2}
+{ headquartersAllocated: 0,
+  headquartersTotal: 0,
+  pakalId: 3,
+  pakalName: "פקל 2",
+  squad1Allocated: 0,
+  squad1Total: 1,
+  squad2Allocated: 0,
+  squad2Total: 2,
+  squad3Allocated: 0,
+  squad3Total: 3,
+  supportAllocated: 0,
+  supportTotal: 0,
+  taskId: 3,
+  warehouseTotal: 2}
   ]; 
   constructor(private requestsService: RequestsService) { }
 
   ngOnInit(): void {
-    this.reloadPakalAllocated();
+    //this.reloadPakalAllocated();
   }
 
   async reloadPakalAllocated(){
